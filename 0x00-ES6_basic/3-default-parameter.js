9dd8c98d6c595919d420bb1ea438ba4f173ec3e7
daniel <ruizdani301@gmail.com>
2022-07-25 16:08:24 -0500

    task-3

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,8 @@
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    if (expansion1989 === undefined) {
+    }
+  
+    if (expansion2019 === undefined) {
+    }
+    return initialNumber + expansion1989 + expansion2019;
+}
