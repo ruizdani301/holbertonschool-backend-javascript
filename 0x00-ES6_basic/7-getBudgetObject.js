d6ae2e4c31f11ad5a828508bc93ef0fa353c7b8f
daniel <ruizdani301@gmail.com>
2022-07-25 22:57:08 -0500

    task-7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,4 @@
-export default function getBudgetObject(income, gdp, capita) {
-    let budget = `{income: ${income}, gdp: ${gdp}, capita: ${capita}}`
-
-    return budget;
+export default function getBudgetObject (income, gdp, capita) {
+  const budget = { income, gdp, capita };
+  return budget;
 }
