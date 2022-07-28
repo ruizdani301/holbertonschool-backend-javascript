fa4b1d643e001458a8577959a44d8cb269263088
daniel <ruizdani301@gmail.com>
2022-07-27 23:58:39 -0500

    task-9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+	const queue = [];
+	try {
+		queue.push(mathFunction)
+	}
+	catch {
+		queue.push('Error: cannot divide by 0')
+	}
+	finally {
+		queue.push('Guardrail was processed')
+	}
+}
