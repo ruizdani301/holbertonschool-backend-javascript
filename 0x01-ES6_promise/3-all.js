b7989a574d01959c5efbcd7ebcbfd8dd9adf86f7
daniel <ruizdani301@gmail.com>
2022-07-27 21:29:43 -0500

    task-3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ export function handleProfileSignup() {
 	Promise.all([uploadPhoto(), createUser()])
 		.then((result) => {
 			console.log(
-				`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+				result[0].body + ' ' + result[1].firstName + ' ' + result[1].lastName);
 		})
 		.catch(() => {
 			console.log("Signup system offline");
