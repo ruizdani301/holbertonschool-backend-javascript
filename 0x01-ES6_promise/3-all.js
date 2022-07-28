42b9cded9a29d51c8bb723788caeb72d03f6a49e
daniel <ruizdani301@gmail.com>
2022-07-27 20:59:24 -0500

    task-3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from "./utils"
 export default function handleProfileSignup() {
 	Promise.all([uploadPhoto, createUser])
 		.then((result) => console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`))
-		.catch(() => console.log(`Signup system offline`));
+		.catch(() => console.log("Signup system offline"));
 }
