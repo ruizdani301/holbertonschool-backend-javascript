b4296e22bffbb1d4be46a29e9d611630f81fc350
daniel <ruizdani301@gmail.com>
2022-07-28 00:21:49 -0500

    task-9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,7 @@
 export default function guardrail(mathFunction) {
 	const queue = [];
 	try {
-		queue.push(mathFunction)
+		queue.push(mathFunction())
 	} catch (error) {
 		queue.push(`Error: ${error.message}`);
 	} finally {
