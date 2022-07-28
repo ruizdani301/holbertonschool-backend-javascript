4874974157b89a7c1d921c136469e9d505c10abd
daniel <ruizdani301@gmail.com>
2022-07-27 21:57:35 -0500

    corretccion de string

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-	Promise.all([uploadPhoto(), createUser()])
-		.then((result) => {
-			console.log(
-				result[0].body + ' ' + result[1].firstName + ' ' + result[1].lastName
-			);
-		})
-		.catch(() => {
-			console.log('Signup system offline');
-		});
+export default function handleProfileSignup () {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(
+        result[0].body + ' ' + result[1].firstName + ' ' + result[1].lastName
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
