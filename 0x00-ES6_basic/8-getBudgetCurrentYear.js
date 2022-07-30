db7f977e9b5e796ea4a06622c3d4f1855b06b642
ruizdani301 <ruizdani301@gmail.com>
2022-07-30 02:34:12 -0500

    task-8

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,13 +1,13 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
+  const date = new Date();
+  return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    return {
+  return {
 
-        [`income-${getCurrentYear()}`]: income,
-        [`gdp-${getCurrentYear()}`]: gdp,
-        [`capita-${getCurrentYear()}`]: capita,
-    };
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
