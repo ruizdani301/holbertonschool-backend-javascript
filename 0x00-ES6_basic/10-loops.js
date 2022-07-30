7a0af0a7fb094a177db23ad2a671df714acdfaca
ruizdani301 <ruizdani301@gmail.com>
2022-07-30 02:36:38 -0500

    task-10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let arreglo = [];
-    for (const idx of array) {
-        arreglo.push(appendString + idx);
-    }
-
-    return arreglo;
+  const arreglo = [];
+  for (const idx of array) {
+    arreglo.push(appendString + idx);
+  }
+  return arreglo;
 }
