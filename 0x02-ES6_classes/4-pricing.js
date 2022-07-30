81ac0d7b43a46b91180144a41c7168e13ba10db9
ruizdani301 <ruizdani301@gmail.com>
2022-07-30 00:12:43 -0500

    task-4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(dato) {
+    this._amount = dato;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(dato1) {
+    this._currency = dato1;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
