4a47d14ba838688b00a90f7986786b31ec047532
ruizdani301 <ruizdani301@gmail.com>
2022-07-30 17:23:59 -0500

    task-5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,19 @@
 export default class Building {
-  constructor(sqft){
+  constructor(sqft) {
     this._sqft = sqft;
     this.evacuationWarningMessage();
   }
+
   evacuationWarningMessage() {
     if (this.constructor.name !== 'Building') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
+
   get sqft() {
     return this.sqft;
   }
-  
+
   set sqft(sqft) {
     this._sqft = sqft;
   }
