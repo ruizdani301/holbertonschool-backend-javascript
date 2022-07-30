37514e1aecac84fd44a5513cae0b3cd5ceb67096
ruizdani301 <ruizdani301@gmail.com>
2022-07-30 02:42:33 -0500

    task-100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,4 @@
-export default function createIteratorObject (report) {
+export default function createIteratorObject(report) {
   let lista = [];
   for (const emp of Object.values(report.allEmployees)) {
     lista = lista.concat(emp);
