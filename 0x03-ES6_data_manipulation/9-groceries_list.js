025496a34b46ac32358493372aefd4958ef6db0e
ruizdani301 <ruizdani301@gmail.com>
2022-08-02 22:33:39 -0500

    task-9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mapeo = new Map();
+  mapeo.set('Apples', 10);
+  mapeo.set('Tomatoes', 10);
+  mapeo.set('Pasta', 1);
+  mapeo.set('Rice', 1);
+  mapeo.set('Banana', 5);
+  return mapeo;
+}
